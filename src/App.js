7dbbc93e808c25e940524661ee645cd5d8b7ce4f
Saurabh ojha <107386248+saurabhojhacoder@users.noreply.github.com>
2023-10-11 17:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from'./components/Navbar';
+import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
@@ -7,60 +7,59 @@ import Alert from './components/Alert';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] =useState('light'); //whether the dark mode is enabled or not
-  const [alert,setAlert]=useState(null)
+  const [mode, setMode] = useState('light'); // Whether the dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
-    setTimeout(()=>{
-        setAlert(null);
-    },3000)
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-info');
+    document.body.classList.remove('bg-primary');
   }
 
-  const removeBodyClasses = ()=>{
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-success')
-    document.body.classList.remove('bg-info')
-    document.body.classList.remove('bg-primary')
-}
+  const toggleMode = (palette) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-' + palette.trim()); // Use trim() to remove any leading/trailing spaces.
 
-const togglemode = (palette) => {
-  removeBodyClasses();
-  document.body.classList.add('bg-' + palette.trim()); // Use trim() to remove any leading/trailing spaces.
-
-  if (mode=== 'light' ,'warning', 'danger', 'success' , 'info' , 'primary'){
-    setMode('dark');
+    if (palette === 'dark') {
+      setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Pallete Changed", "success");
+      showAlert("Palette Changed", "success");
       document.title = "Word Converter - Dark Mode";
-  } 
-  else {
-    setMode('light');
-        document.body.style.backgroundColor = 'white';  
-        showAlert("Light mode is enabled", "success");
-        document.title = "Word Converter - Light Mode";
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is enabled", "success");
+      document.title = "Word Converter - Light Mode";
+    }
   }
-}
 
   return (
     <>
-    <Router>
-    <Navbar title="Word Counter" about="About us" mode={mode} togglemode={togglemode}/>
-    <Alert alert={alert}/>
-    <div className="container my-3">
-      <Routes>
-      <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Text Converter" mode={mode} />} />
-      <Route exact path='/about' element={<About mode={mode} />} />
-
-      </Routes>
-    </div>
-    </Router> 
+      <Router>
+        <Navbar title="Word Counter" about="About us" mode={mode} togglemode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path='/' element={<TextForm showAlert={showAlert} heading="Text Converter" mode={mode} />} />
+            <Route path='/about' element={<About mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
-};
-export default App;  
+}
+
+export default App;
